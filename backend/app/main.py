f1ff6a315eb4f3a3a85ef46bd4e26cc05a27ce30
Abhinav Garg <gargabhinav77@gmail.com>
2026-05-28 15:53:00 +0530

    Connect FastAPI backend with MongoDB Atlas

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.config.db import db
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "FluentVeda Backend Running"}
+
+@app.get("/test-db")
+def test_db():
+    return {
+        "collections": db.list_collection_names()
+    }
